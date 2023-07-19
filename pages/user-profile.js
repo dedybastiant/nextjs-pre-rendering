8c1d5ba493bf39d989dbff9ffcd35d9dd67fe16b
dedybastiant <dedybastian.t@gmail.com>
2023-07-19 17:17:04 +0700

    Learning about getSeverSideProps and use it withdynamic param

---
+++ pages/user-profile.js
@@ -0,0 +1,13 @@
+function UserProfile(props) {
+  return <h1>{props.username}</h1>;
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      username: "DEDY",
+    },
+  };
+}
+
+export default UserProfile;
