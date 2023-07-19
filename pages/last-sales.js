e48e95d1b63e1a20778c9c25cbc41c0d3df8169e
dedybastiant <dedybastian.t@gmail.com>
2023-07-20 06:37:41 +0700

    Learn t combine pre fetching and client side fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-function LastSales() {
-  const [sales, setSales] = useState();
+function LastSales(props) {
+  const [sales, setSales] = useState(props.sales);
   // const [isLoading, setIsLoading] = useState(false);
 
   const fetcher = (url) => fetch(url).then((response) => response.json());
@@ -54,19 +54,37 @@ function LastSales() {
     return <p>Failed to load.</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading</p>;
   }
 
   return (
     <ul>
       {sales.map((sale) => (
-        <li>
+        <li key={sale.id}>
           {sale.username} - ${sale.volume}
         </li>
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextproject-f03d3-default-rtdb.asia-southeast1.firebasedatabase.app/sales.json"
+  );
+
+  const data = await response.json();
+  const transformedSales = [];
+
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      username: data[key].username,
+      volume: data[key].volume,
+    });
+  }
+  return { props: { sales: transformedSales, revalidate: 10 } };
 }
 
 export default LastSales;
